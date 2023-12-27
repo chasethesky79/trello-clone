cfcc912a5b7dc72dd3a8b960142740c3b3229532
chasethesky79 <seshadri.bharath@yahoo.com>
2023-12-26 18:42:01 -0600

    Implemented the useFocus custom hook

--- src/components/NewItemForm.tsx
+++ src/components/NewItemForm.tsx
@@ -1,14 +1,17 @@
 import { useState } from "react";
 import { NewItemFormContainer, NewItemInput, NewItemButton } from "../styles";
+import { useFocus } from "../utils/useFocus";
 
 type NewItemFormProps = {
     onAdd: (text: string) => void
 }
 export const NewItemForm = ({ onAdd }: NewItemFormProps ) => {
     const [text, setText] = useState('')
+    const inputRef = useFocus();
+
     return (
         <NewItemFormContainer>
-            <NewItemInput onChange={(e: React.ChangeEvent<HTMLInputElement>) => setText(e.target.value)}/>
+            <NewItemInput ref={inputRef} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setText(e.target.value)}/>
             <NewItemButton onClick={() => onAdd(text)}>Create Item</NewItemButton>
         </NewItemFormContainer>
     )
--- src/styles.ts
+++ src/styles.ts
@@ -74,6 +74,6 @@ export const NewItemInput = styled.input`
     box-shadow: #091e4240 0px 1px 0px 0px;
     margin-bottom: 0.5rem;
     padding: 0.5rem 1rem;
-    width: 100%;
+    width: 90%;
 `
 export {}
