e7e364b9df018c82d5d5c1417aa9604e805880b9
chasethesky79 <seshadri.bharath@yahoo.com>
2023-12-31 11:46:13 -0600

    Added logic to handle the create item button click

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { ColumnContainer, ColumnTitle } from '../styles';
 import { AddNewItem } from './AddNewItem';
 import { Card } from './Card';
@@ -7,13 +8,12 @@ type ColumnProps = {
 }
 
 export const Column = ({ text }: ColumnProps) => {
+    const [items, setItems] = useState<Array<string>>(['Generate App Scaffold', 'Learn Typescript', 'Begin to use static typing']);
     return (
         <ColumnContainer>
         <ColumnTitle>{ text }</ColumnTitle>
-            <Card text='Generate App Scaffold'></Card>
-            <Card text='Learn Typescript'></Card>
-            <Card text='Begin to use static typing'></Card>
-            <AddNewItem dark={true} toggleButtonText='+ Add another card' onAdd={() => console.log(`New Item Added`)}/>
+            {items?.map(item => <Card text={item}/>)}
+            <AddNewItem dark={true} toggleButtonText='+ Add another card' onAdd={(text) => setItems([...items, text])}/>
         </ColumnContainer>
     )
 }
--- src/components/NewItemForm.tsx
+++ src/components/NewItemForm.tsx
@@ -8,10 +8,16 @@ type NewItemFormProps = {
 export const NewItemForm = ({ onAdd }: NewItemFormProps ) => {
     const [text, setText] = useState('')
     const inputRef = useFocus();
+    const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if(event.key === 'Enter'){
+          onAdd(text)
+        }
+    }
 
     return (
         <NewItemFormContainer>
-            <NewItemInput ref={inputRef} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setText(e.target.value)}/>
+            <NewItemInput ref={inputRef} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setText(e.target.value)} 
+                onKeyPress={handleKeyPress}/>
             <NewItemButton onClick={() => onAdd(text)}>Create Item</NewItemButton>
         </NewItemFormContainer>
     )
