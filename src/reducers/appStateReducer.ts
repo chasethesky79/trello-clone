6332ec930055b16ab616ab24342110617b5b935e
chasethesky79 <seshadri.bharath@yahoo.com>
2024-01-01 14:14:36 -0600

    Implemented id generation with nanoid

--- src/reducers/appStateReducer.ts
+++ src/reducers/appStateReducer.ts
@@ -1,12 +1,13 @@
 import { Action, ActionType, AddTaskAction } from "../state/actions";
 import { AppState, List } from "../state/AppStateContext";
+import { nanoid } from 'nanoid'
 
 export default function appReducer(state: AppState, action: Action): AppState {
    switch(action.type) {
      case ActionType.ADD_LIST:
         return {
             ...state,
-            lists: [...state.lists, { text: action.payload, id: Math.random().toString()}]
+            lists: [...state.lists, { text: action.payload, id: nanoid()}]
         }
     case ActionType.ADD_TASK:
         const { text, listId } = (action as AddTaskAction)?.payload
@@ -16,7 +17,7 @@ export default function appReducer(state: AppState, action: Action): AppState {
                 if (list.id === listId) {
                     return {
                         ...list,
-                        tasks: list?.tasks ? [...list.tasks, { text, listId, id: Math.random().toString()}] : []
+                        tasks: list?.tasks ? [...list.tasks, { text, listId, id: nanoid()}] : []
                     }
                 }
                 return list
